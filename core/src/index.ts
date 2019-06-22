fd36780129db92894fd4ae3d826579ea535c3317
Krzysztof Borowy <krizzu.dev@gmail.com>
2019-06-22 12:41:29 +0200

    feat: initial implementation

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,1 +1,17 @@
-export {default as AsyncStorage} from './AsyncStorage';
+import AsyncStorage from './AsyncStorage';
+import {factoryOptions} from './defaults';
+
+class AsyncStorageFactory {
+  constructor() {
+    throw new Error('Do not instantiate AsyncStorageFactory!');
+  }
+
+  static create<STR extends IStorageBackend>(
+    storage: STR,
+    opts: FactoryOptions = factoryOptions,
+  ): AsyncStorage<STR, StorageModelType<STR>> {
+    return new AsyncStorage(storage, opts);
+  }
+}
+
+export default AsyncStorageFactory;
