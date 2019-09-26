17f1993fce7e59b939b3d71343e33d8270fa1fe3
Weigel <jesse.weigel@dcsg.com>
2019-09-26 17:02:33 -0400

    chore: remove webStorage example app (will be added in another PR)

--- examples/web/src/storage.js
+++
@@ -1,8 +0,0 @@
-import WebStorage from '@react-native-community/async-storage-backend-web';
-import AsyncStorageFactory from '@react-native-community/async-storage';
-
-const web = new WebStorage();
-
-const storage = AsyncStorageFactory.create(web, {});
-
-export default storage;
