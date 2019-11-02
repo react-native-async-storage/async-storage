e030755ecc0f06c2b7adc0780f35956e52433bc1
Bartol Karuza <bartol.k@gmail.com>
2019-11-02 18:39:41 +0100

    fix(example): wrong file path in index

--- example/index.js
+++ example/index.js
@@ -9,7 +9,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
