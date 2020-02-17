19ba4d3625a651abeb21838c1999b87b76e30f74
Tom Underhill <tomun@microsoft.com>
2020-02-16 21:36:06 -0800

    Removed escape from string

--- .circleci/scripts/run_macos_e2e.sh
+++ .circleci/scripts/run_macos_e2e.sh
@@ -3,7 +3,7 @@
 RESOURCE_DIR="$PWD/example/ios/build/Build/Products/Release-macosx/AsyncStorageExample-macOS.app"
 ENTRY_FILE="example/index.js"
 BUNDLE_FILE="$RESOURCE_DIR/main.jsbundle"
-EXTRA_PACKAGER_ARGS="--entry-file=$ENTRY_FILE\ --use-react-native-macos"
+EXTRA_PACKAGER_ARGS="--entry-file=$ENTRY_FILE --use-react-native-macos"
 BUILD_ACTIONS="$@"
 
 echo "[XCode e2e] Building macOS project"
