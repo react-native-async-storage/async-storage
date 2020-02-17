3b2d93964b9b53a43385bd94a2742325f1579ae9
Tom Underhill <tomun@microsoft.com>
2020-02-16 21:24:14 -0800

    XCUITests for macOS not supported on CCI.  So just build the example app.

--- .circleci/scripts/run_macos_e2e.sh
+++ .circleci/scripts/run_macos_e2e.sh
@@ -6,21 +6,17 @@ BUNDLE_FILE="$RESOURCE_DIR/main.jsbundle"
 EXTRA_PACKAGER_ARGS="--entry-file=$ENTRY_FILE\ --use-react-native-macos"
 BUILD_ACTIONS="$@"
 
-build_project() {
-  echo "[XCode e2e] Building macOS project"
-  export RCT_NO_LAUNCH_PACKAGER=true
-  eval "xcodebuild \
-    -project example/macos/AsyncStorageExample.xcodeproj \
-    -scheme AsyncStorageExample-macOS \
-    -configuration Release \
-    -sdk macosx \
-    -derivedDataPath example/macos/build \
-    -UseModernBuildSystem=NO \
-    BUNDLE_FILE=$BUNDLE_FILE \
-    EXTRA_PACKAGER_ARGS=$EXTRA_PACKAGER_ARGS \
-    $BUILD_ACTIONS"
-}
+echo "[XCode e2e] Building macOS project"
+export RCT_NO_LAUNCH_PACKAGER=true
+xcodebuild \
+  -project example/macos/AsyncStorageExample.xcodeproj \
+  -scheme AsyncStorageExample-macOS \
+  -configuration Release \
+  -sdk macosx \
+  -derivedDataPath example/macos/build \
+  -UseModernBuildSystem=NO \
+  BUNDLE_FILE="$BUNDLE_FILE" \
+  EXTRA_PACKAGER_ARGS="$EXTRA_PACKAGER_ARGS" \
+  $BUILD_ACTIONS
 
-build_project
-
-exit 0
+exit $?
