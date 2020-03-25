a99d805431bc59c8a7f5ebbf85f71323c83df122
Evan Bacon <baconbrix@gmail.com>
2020-03-25 10:10:53 -0700

    Added web support to legacy test

--- example/index.js
+++ example/index.js
@@ -8,8 +8,13 @@
  * @flow
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (Platform.OS === 'web') {
+  const rootTag = document.getElementById('root');
+  AppRegistry.runApplication(appName, {rootTag});
+}
