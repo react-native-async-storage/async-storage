9f137980a3bd750bb6b537022c1613870e0c06ca
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-08-12 19:32:02 +0200

    Add support for enabled TurboModules in Expo

--- src/RCTAsyncStorage.expo.js
+++ src/RCTAsyncStorage.expo.js
@@ -7,7 +7,18 @@
  * will likely not be valid anymore, and the package will need to be included in the Expo SDK
  * to continue to work.
  */
-const {NativeModules} = require('react-native');
-const RCTAsyncStorage = NativeModules.AsyncSQLiteDBStorage || NativeModules.AsyncLocalStorage;
+const {NativeModules, TurboModuleRegistry} = require('react-native');
+
+let RCTAsyncStorage;
+
+// TurboModuleRegistry falls back to NativeModules
+// so we don't have to try go assign NativeModules'
+// counterparts if TurboModuleRegistry would resolve
+// with undefined.
+if (TurboModuleRegistry) {
+  RCTAsyncStorage = TurboModuleRegistry.get("AsyncSQLiteDBStorage") || TurboModuleRegistry.get("AsyncLocalStorage");
+} else {
+  RCTAsyncStorage = NativeModules.AsyncSQLiteDBStorage || NativeModules.AsyncLocalStorage;
+}
 
 export default RCTAsyncStorage;
