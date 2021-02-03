4946614afbf8098f0786e97268ccd049cf6755d1
Krzysztof Borowy <krizzu.dev@gmail.com>
2021-02-03 10:40:56 +0100

    change flag name

--- android/src/main/java/com/reactnativecommunity/asyncstorage/AsyncStoragePackage.java
+++ android/src/main/java/com/reactnativecommunity/asyncstorage/AsyncStoragePackage.java
@@ -24,7 +24,7 @@ public class AsyncStoragePackage implements ReactPackage {
 
         List<NativeModule> moduleList = new ArrayList<>(1);
 
-        if (BuildConfig.AsyncStorage_useRoomLibrary) {
+        if (BuildConfig.AsyncStorage_useNext) {
             try {
                 Class storageClass = Class.forName("com.reactnativecommunity.asyncstorage.next.StorageModule");
                 NativeModule inst = (NativeModule) storageClass.getDeclaredConstructor(new Class[]{ReactContext.class}).newInstance(reactContext);
