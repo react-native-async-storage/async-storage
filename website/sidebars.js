89f47c7f0255285eb041020ea90f1f4830590abb
lbaldy <lukaszbaldy@gmail.comy>
2021-09-19 23:34:37 +0200

    docs(website): add debugging section with community packages
    add community packages inside the debugging section listing the packages helpful for debugging for
    Flipper and Non-Flipper projects
    
    re #669

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,6 +2,7 @@ module.exports = {
   docs: {
     'Getting started': ['install', 'usage', 'link', 'api'],
     Advanced: ['advanced/next', 'advanced/jest', 'advanced/brownfield', 'advanced/backup', 'advanced/executor', 'advanced/db_size'],
+    Debugging: ['debugging/communityPackages'],
     Help: ['help/troubleshooting'],
   },
 };
