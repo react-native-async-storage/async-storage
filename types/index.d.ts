e028a3aed3cda9e221d44923acfb76fec7bda8b6
DeokHyeon Wi <dnl9807@gmail.com>
2021-11-09 14:35:30 +0900

    add type definition for jest mock

--- types/index.d.ts
+++ types/index.d.ts
@@ -86,3 +86,34 @@ declare module '@react-native-async-storage/async-storage' {
 
   export default AsyncStorage;
 }
+
+declare module '@react-native-async-storage/async-storage/jest/async-storage-mock' {
+   export interface AsyncStorageStatic {
+    getItem(key: string, callback?: (error?: Error, result?: string) => void): Promise<string | null>;
+    setItem(key: string, value: string, callback?: (error?: Error) => void): Promise<void>;
+    removeItem(key: string, callback?: (error?: Error) => void): Promise<void>;
+    mergeItem(key: string, value: string, callback?: (error?: Error) => void): Promise<void>;
+
+    clear(callback?: (error?: Error) => void): Promise<void>;
+    getAllKeys(callback?: (error?: Error, keys?: string[]) => void): Promise<string[]>;
+
+    multiGet(
+      keys: string[],
+      callback?: (errors?: Error[], result?: [string, string | null][]) => void
+    ): Promise<[string, string | null][]>;
+    multiSet(keyValuePairs: string[][], callback?: (errors?: Error[]) => void): Promise<void>;
+    multiRemove(keys: string[], callback?: (errors?: Error[]) => void): Promise<void>;
+    multiMerge(keyValuePairs: string[][], callback?: (errors?: Error[]) => void): Promise<void>;
+  }
+
+  export function useAsyncStorage(key: string): {
+    getItem(callback?: (error?: Error, result?: string) => void): Promise<string | null>;
+    setItem(value: string, callback?: (error?: Error) => void): Promise<void>;
+    mergeItem(value: string, callback?: (error?: Error) => void): Promise<void>;
+    removeItem(callback?: (error?: Error) => void): Promise<void>;
+  }
+
+  const AsyncStorage: AsyncStorageStatic;
+
+  export default AsyncStorage;
+}
