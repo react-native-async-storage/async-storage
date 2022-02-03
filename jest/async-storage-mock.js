085cc4c80d9b596cda83972deab7010f88f88a5b
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-02-03 21:03:17 +0100

    fixup! feat: migrate from Flow to TypeScript

--- jest/async-storage-mock.js
+++ jest/async-storage-mock.js
@@ -28,7 +28,7 @@ const asMock = {
 
   removeItem: jest.fn((key, callback) => asMock.multiRemove([key], callback)),
   mergeItem: jest.fn((key, value, callback) =>
-    asMock.multiMerge([[key, value]], callback),
+    asMock.multiMerge([[key, value]], callback)
   ),
 
   clear: jest.fn(_clear),
