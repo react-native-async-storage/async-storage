aa47ad3986c39d7527b3cec00faa6cd2358fc18b
Mattia Asti <mtt87@users.noreply.github.com>
2022-03-09 17:20:00 +0000

    Update types.ts
    Fix https://github.com/react-native-async-storage/async-storage/issues/764

--- src/types.ts
+++ src/types.ts
@@ -125,7 +125,7 @@ export type AsyncStorageStatic = {
   multiGet: (
     keys: string[],
     callback?: MultiGetCallback
-  ) => Promise<readonly KeyValuePair[] | void>;
+  ) => Promise<readonly KeyValuePair[]>;
 
   /**
    * Use this as a batch operation for storing multiple key-value pairs. When
