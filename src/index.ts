db285138137b7d213598961b672be6ed8fade754
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-05-18 14:34:43 +0200

    fix: invalid module name in augmentation (#805)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
 import AsyncStorage from './AsyncStorage';
-import type { AsyncStorageHook, AsyncStorageStatic } from './types';
 
 export { useAsyncStorage } from './hooks';
-export type { AsyncStorageStatic } from './types';
 
 export default AsyncStorage;
-
-// @ts-ignore AsyncStorage mock module
-declare module '@react-native-async-storage/async-storage/jest/async-storage-mock' {
-  export function useAsyncStorage(key: string): AsyncStorageHook;
-  const AsyncStorageLib: AsyncStorageStatic;
-  export default AsyncStorageLib;
-}
