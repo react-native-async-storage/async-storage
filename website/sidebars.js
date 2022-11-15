b5f246676b5cdbe9b7496a8388c648b2e961ae90
Krzysztof Borowy <krizzu.dev@gmail.com>
2022-11-15 15:36:15 +0100

    fix website deployment

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,7 +1,14 @@
 module.exports = {
   docs: {
-    'Getting started': ['install', 'usage', 'link', 'api', 'limits'],
-    Advanced: ['advanced/next', 'advanced/jest', 'advanced/brownfield', 'advanced/backup', 'advanced/executor', 'advanced/db_size'],
+    'Getting started': ['install', 'usage', 'api', 'limits'],
+    Advanced: [
+      'advanced/next',
+      'advanced/jest',
+      'advanced/brownfield',
+      'advanced/backup',
+      'advanced/executor',
+      'advanced/db_size',
+    ],
     Debugging: ['debugging/communityPackages'],
     Help: ['help/troubleshooting'],
   },
