8d473c0009fe1c797709656816de87d69cdd34b8
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-11-15 16:09:12 +0100

    docs: remove Linking from the side bar (#875)

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   docs: {
-    'Getting started': ['install', 'usage', 'link', 'api', 'limits'],
+    'Getting started': ['install', 'usage', 'api', 'limits'],
     Advanced: ['advanced/next', 'advanced/jest', 'advanced/brownfield', 'advanced/backup', 'advanced/executor', 'advanced/db_size'],
     Debugging: ['debugging/communityPackages'],
     Help: ['help/troubleshooting'],
