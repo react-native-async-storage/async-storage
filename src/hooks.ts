59592cceb9bd0e527ff16d716431bdf7b7510aa4
Daomtthuan <dao.mt.thuan@gmail.com>
2022-11-28 14:17:51 +0700

    Update hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,13 +1,15 @@
 import { useMemo } from 'react';
+
 import AsyncStorage from './AsyncStorage';
-import type { AsyncStorageHook } from './types';
+
+import type { AsyncStorageHook, Callback, CallbackWithResult } from './types';
 
 export function useAsyncStorage(key: string): AsyncStorageHook {
   const asyncStorage = useMemo(() => ({
-    getItem: (...args) => AsyncStorage.getItem(key, ...args),
-    setItem: (...args) => AsyncStorage.setItem(key, ...args),
-    mergeItem: (...args) => AsyncStorage.mergeItem(key, ...args),
-    removeItem: (...args) => AsyncStorage.removeItem(key, ...args),
+    getItem: (callback?: CallbackWithResult<string>) => AsyncStorage.getItem(key, callback),
+    setItem: (value: string, callback?: Callback) => AsyncStorage.setItem(key, value, callback),
+    mergeItem: (value: string, callback?: Callback) => AsyncStorage.mergeItem(key, value, callback),
+    removeItem: (callback?: Callback) => AsyncStorage.removeItem(key, callback),
   }), [key]);
   
   return asyncStorage;
