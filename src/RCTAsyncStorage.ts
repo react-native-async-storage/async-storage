ebbb5cf96b617b2b7ccf634265c4497a57cc65b6
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-03-13 14:07:32 +0100

    refactor: add TurboModule spec
    We don't have a TurboModule implementation yet, but this should be
    backwards compatible with legacy native module.

--- src/RCTAsyncStorage.ts
+++ src/RCTAsyncStorage.ts
@@ -1,25 +1,66 @@
-// @ts-ignore Module '"react-native"' has no exported member 'TurboModuleRegistry'.
-import { NativeModules, TurboModuleRegistry } from 'react-native';
+import type { TurboModule } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
 import { shouldFallbackToLegacyNativeModule } from './shouldFallbackToLegacyNativeModule';
+import type { ErrorLike } from './types';
 
-let RCTAsyncStorage =
-  NativeModules['PlatformLocalStorage'] || // Support for external modules, like react-native-windows
-  NativeModules['RNC_AsyncSQLiteDBStorage'] ||
-  NativeModules['RNCAsyncStorage'];
+export interface Spec extends TurboModule {
+  clear(callback: (error?: ErrorLike) => void): void;
 
-if (!RCTAsyncStorage && shouldFallbackToLegacyNativeModule()) {
-  // TurboModuleRegistry falls back to NativeModules so we don't have to try go
-  // assign NativeModules' counterparts if TurboModuleRegistry would resolve
-  // with undefined.
-  if (TurboModuleRegistry) {
-    RCTAsyncStorage =
-      TurboModuleRegistry.get('AsyncSQLiteDBStorage') ||
-      TurboModuleRegistry.get('AsyncLocalStorage');
-  } else {
-    RCTAsyncStorage =
-      NativeModules['AsyncSQLiteDBStorage'] ||
-      NativeModules['AsyncLocalStorage'];
-  }
+  getAllKeys(callback: (error?: ErrorLike, keys?: string[]) => void): void;
+
+  multiGet(
+    keys: readonly string[],
+    callback: (errors?: ErrorLike[], result?: [string, string | null][]) => void
+  ): void;
+
+  multiMerge: (
+    keyValuePairs: readonly [string, string][],
+    callback: (errors?: ErrorLike[]) => void
+  ) => Promise<void>;
+
+  multiRemove(
+    strings: readonly string[],
+    callback: (errors?: ErrorLike[]) => void
+  ): void;
+
+  multiSet(
+    keyValuePairs: readonly [string, string][],
+    callback: (errors?: ErrorLike[]) => void
+  ): void;
 }
 
-export default RCTAsyncStorage;
+function getModule(): Spec {
+  const nativeModuleNames = [
+    'PlatformLocalStorage', // Support for external modules, like react-native-windows
+    'RNC_AsyncSQLiteDBStorage',
+    'RNCAsyncStorage',
+  ];
+
+  if (shouldFallbackToLegacyNativeModule()) {
+    nativeModuleNames.push('AsyncSQLiteDBStorage', 'AsyncLocalStorage');
+  }
+
+  for (const name of nativeModuleNames) {
+    const impl = TurboModuleRegistry.get<Spec>(name);
+    if (impl) {
+      return impl;
+    }
+  }
+
+  throw new Error(`[@RNC/AsyncStorage]: NativeModule: AsyncStorage is null.
+
+To fix this issue try these steps:
+
+  • Rebuild and restart the app.
+
+  • Run the packager with \`--reset-cache\` flag.
+
+  • If you are using CocoaPods on iOS, run \`pod install\` in the \`ios\` directory and then rebuild and re-run the app.
+
+  • If this happens while testing with Jest, check out docs how to integrate AsyncStorage with it: https://react-native-async-storage.github.io/async-storage/docs/advanced/jest
+
+If none of these fix the issue, please open an issue on the Github repository: https://github.com/react-native-async-storage/async-storage/issues
+`);
+}
+
+export default getModule();
