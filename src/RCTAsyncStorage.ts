62732d900f6dd2cc5b4c14230f33ed32114d6a09
Eric Rozell <erozell@outlook.com>
2023-06-06 04:30:09 -0400

    fix: allow consumption of `RCTAsyncStorage` as TurboModule (#965)
    In react-native-windows, attributed C++ modules can be consumed as
    either a legacy native module or TurboModule. This change enables
    registration of RNCAsyncStorage as a TurboModule.

--- src/RCTAsyncStorage.ts
+++ src/RCTAsyncStorage.ts
@@ -2,15 +2,18 @@
 import { NativeModules, TurboModuleRegistry } from 'react-native';
 import { shouldFallbackToLegacyNativeModule } from './shouldFallbackToLegacyNativeModule';
 
-let RCTAsyncStorage =
-  NativeModules['PlatformLocalStorage'] || // Support for external modules, like react-native-windows
-  NativeModules['RNC_AsyncSQLiteDBStorage'] ||
-  NativeModules['RNCAsyncStorage'];
+// TurboModuleRegistry falls back to NativeModules so we don't have to try go
+// assign NativeModules' counterparts if TurboModuleRegistry would resolve
+// with undefined.
+let RCTAsyncStorage = TurboModuleRegistry
+  ? TurboModuleRegistry.get('PlatformLocalStorage') || // Support for external modules, like react-native-windows
+    TurboModuleRegistry.get('RNC_AsyncSQLiteDBStorage') ||
+    TurboModuleRegistry.get('RNCAsyncStorage')
+  : NativeModules['PlatformLocalStorage'] || // Support for external modules, like react-native-windows
+    NativeModules['RNC_AsyncSQLiteDBStorage'] ||
+    NativeModules['RNCAsyncStorage'];
 
 if (!RCTAsyncStorage && shouldFallbackToLegacyNativeModule()) {
-  // TurboModuleRegistry falls back to NativeModules so we don't have to try go
-  // assign NativeModules' counterparts if TurboModuleRegistry would resolve
-  // with undefined.
   if (TurboModuleRegistry) {
     RCTAsyncStorage =
       TurboModuleRegistry.get('AsyncSQLiteDBStorage') ||
