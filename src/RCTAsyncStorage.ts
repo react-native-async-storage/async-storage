7bcb08b28fccdc2739b9879888096e70247c15f1
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-06-21 15:03:11 +0200

    Revert module resolution

--- src/RCTAsyncStorage.ts
+++ src/RCTAsyncStorage.ts
@@ -2,12 +2,16 @@
 import { NativeModules, TurboModuleRegistry } from 'react-native';
 import { shouldFallbackToLegacyNativeModule } from './shouldFallbackToLegacyNativeModule';
 
-import AsyncStorageTurboModule from './NativeAsyncStorageModule';
-
-let RCTAsyncStorage =
-  AsyncStorageTurboModule ||
-  NativeModules['PlatformLocalStorage'] || // Support for external modules, like react-native-windows
-  NativeModules['RNCAsyncStorage'];
+// TurboModuleRegistry falls back to NativeModules so we don't have to try go
+// assign NativeModules' counterparts if TurboModuleRegistry would resolve
+// with undefined.
+let RCTAsyncStorage = TurboModuleRegistry
+  ? TurboModuleRegistry.get('PlatformLocalStorage') || // Support for external modules, like react-native-windows
+    TurboModuleRegistry.get('RNC_AsyncSQLiteDBStorage') ||
+    TurboModuleRegistry.get('RNCAsyncStorage')
+  : NativeModules['PlatformLocalStorage'] || // Support for external modules, like react-native-windows
+    NativeModules['RNC_AsyncSQLiteDBStorage'] ||
+    NativeModules['RNCAsyncStorage'];
 
 if (!RCTAsyncStorage && shouldFallbackToLegacyNativeModule()) {
   if (TurboModuleRegistry) {
