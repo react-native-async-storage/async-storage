c770d4d9e15cf56f653bf85c2c8916df272b90e5
Douglas Lowder <douglowder@mac.com>
2023-11-01 03:04:59 -0700

    fix(tvos): Apple TV warning should only happen once (#1021)

--- packages/default-storage-backend/ios/RNCAsyncStorage.mm
+++ packages/default-storage-backend/ios/RNCAsyncStorage.mm
@@ -155,6 +155,10 @@ static NSString *RCTGetStorageDirectory()
     static NSString *storageDirectory = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
+#if TARGET_OS_TV
+      RCTLogWarn(
+        @"Persistent storage is not supported on tvOS, your data may be removed at any point.");
+#endif
       storageDirectory = RCTCreateStorageDirectoryPath(RCTStorageDirectory);
     });
     return storageDirectory;
@@ -498,11 +502,6 @@ RCT_EXPORT_MODULE()
 - (NSDictionary *)_ensureSetup
 {
     RCTAssertThread(RCTGetMethodQueue(), @"Must be executed on storage thread");
-
-#if TARGET_OS_TV
-    RCTLogWarn(
-        @"Persistent storage is not supported on tvOS, your data may be removed at any point.");
-#endif
 
     NSError *error = nil;
     if (!RCTHasCreatedStorageDirectory) {
