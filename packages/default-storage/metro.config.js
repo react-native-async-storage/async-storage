59235dec91018aec464f9ff7a908ffefbb60e292
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-06-05 12:08:37 +0200

    chore: bump `react-native` to 0.73 (#1098)

--- packages/default-storage/metro.config.js
+++ packages/default-storage/metro.config.js
@@ -1,26 +1,18 @@
-/**
- * This cli config is needed for development purposes, e.g. for running
- * integration tests during local development or on CI services.
- */
-
-const exclusionList = require("metro-config/src/defaults/exclusionList");
-const path = require("path");
-
-module.exports = {
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+module.exports = makeMetroConfig({
   projectRoot: `${__dirname}/example`,
   watchFolders: [__dirname],
   resolver: {
-    blockList: exclusionList([
-      // This stops "react-native run-windows" from causing the metro server to crash if its already running
-      new RegExp(
-        `${path
-          .resolve(__dirname, "example", "windows")
-          .replace(/[/\\]/g, "/")}.*`
-      ),
-
-      // Workaround for `EBUSY: resource busy or locked, open '~\msbuild.ProjectImports.zip'`
-      // when building with `yarn windows --release`
-      /.*\.ProjectImports\.zip/,
-    ]),
+    extraNodeModules: {
+      "@react-native-async-storage/async-storage": __dirname,
+    },
   },
-};
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+});
