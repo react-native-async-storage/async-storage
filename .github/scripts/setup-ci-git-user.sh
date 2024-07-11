ed4e62968ae56535d2e2e0075da5a4a85040bfe6
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2024-07-11 16:28:25 +0200

    fix: Make sure our bot is used for released (#1116)
    credentials

---
+++ .github/scripts/setup-ci-git-user.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -z ${CI+x} ]
+then
+  # not on CI
+  exit 0
+fi
+
+if [ -z ${GITHUB_NAME+x} ] || [ -z ${GITHUB_EMAIL+x} ] || [ -z ${GITHUB_TOKEN+x} ]
+then
+  echo "Make sure GITHUB_NAME, GITHUB_EMAIL and GITHUB_TOKEN variables are set"
+  exit 1
+fi
+
+git config --global user.name "$GITHUB_NAME"
+git config --global user.email "$GITHUB_EMAIL"
+echo "machine github.com login $GITHUB_NAME password $GITHUB_TOKEN" > ~/.netrc
+
+
