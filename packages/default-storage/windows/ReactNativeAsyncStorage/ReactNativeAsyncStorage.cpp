7e5116e93e365239a448c78a7d73719d785dd7ac
Yajur Grover <yajurgrover24@gmail.com>
2024-08-07 16:55:58 -0700

    add method stubs for turbomodule

--- packages/default-storage/windows/ReactNativeAsyncStorage/ReactNativeAsyncStorage.cpp
+++ packages/default-storage/windows/ReactNativeAsyncStorage/ReactNativeAsyncStorage.cpp
@@ -11,8 +11,28 @@ void ReactNativeAsyncStorage::Initialize(React::ReactContext const &reactContext
   m_context = reactContext;
 }
 
-double ReactNativeAsyncStorage::multiply(double a, double b) noexcept {
-  return a * b;
+void ReactNativeAsyncStorage::multiGet(std::vector<std::string> const & keys, std::function<void(::React::JSValueArray const &, ::React::JSValueArray const &)> const & callback) noexcept {
+  // temporarily unimplemented
+}
+
+void ReactNativeAsyncStorage::multiSet(::React::JSValueArray && kvPairs, std::function<void(::React::JSValueArray const &)> const & callback) noexcept {
+  // temporarily unimplemented
+}
+
+void ReactNativeAsyncStorage::multiRemove(std::vector<std::string> const & keys, std::function<void(::React::JSValueArray const &)> const & callback) noexcept {
+  // temporarily unimplemented
+}
+
+void ReactNativeAsyncStorage::multiMerge(::React::JSValueArray && kvPairs, std::function<void(::React::JSValueArray const &)> const & callback) noexcept {
+  // temporarily unimplemented
+}
+
+void ReactNativeAsyncStorage::getAllKeys(std::function<void(::React::JSValueArray const &, ::React::JSValueArray const &)> const & callback) noexcept {
+  // temporarily unimplemented
+}
+
+void ReactNativeAsyncStorage::clear(std::function<void(::React::JSValueArray const &)> const & callback) noexcept {
+  // temporarily unimplemented
 }
 
 } // namespace winrt::ReactNativeAsyncStorageAsyncStorage
--- packages/default-storage/windows/ReactNativeAsyncStorage/ReactNativeAsyncStorage.h
+++ packages/default-storage/windows/ReactNativeAsyncStorage/ReactNativeAsyncStorage.h
@@ -21,8 +21,23 @@ struct ReactNativeAsyncStorage
   REACT_INIT(Initialize)
   void Initialize(React::ReactContext const &reactContext) noexcept;
 
-  REACT_SYNC_METHOD(multiply)
-  double multiply(double a, double b) noexcept;
+  REACT_METHOD(multiGet)
+  void multiGet(std::vector<std::string> const & keys, std::function<void(::React::JSValueArray const &, ::React::JSValueArray const &)> const & callback) noexcept;
+
+  REACT_METHOD(multiSet)
+  void multiSet(::React::JSValueArray && kvPairs, std::function<void(::React::JSValueArray const &)> const & callback) noexcept;
+
+  REACT_METHOD(multiRemove)
+  void multiRemove(std::vector<std::string> const & keys, std::function<void(::React::JSValueArray const &)> const & callback) noexcept;
+
+  REACT_METHOD(multiMerge)
+  void multiMerge(::React::JSValueArray && kvPairs, std::function<void(::React::JSValueArray const &)> const & callback) noexcept;
+
+  REACT_METHOD(getAllKeys)
+  void getAllKeys(std::function<void(::React::JSValueArray const &, ::React::JSValueArray const &)> const & callback) noexcept;
+
+  REACT_METHOD(clear)
+  void clear(std::function<void(::React::JSValueArray const &)> const & callback) noexcept;
 
 private:
   React::ReactContext m_context;
