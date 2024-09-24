b01454d16a887459bc2b07e27c9996c613fbb160
Yajur Grover <yajurgrover24@gmail.com>
2024-09-24 11:50:37 -0700

    fix pch.h and untrack ExperimentalFeatures.props

--- packages/default-storage/windows/code/pch.h
+++ packages/default-storage/windows/code/pch.h
@@ -1,13 +1,34 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
+﻿// pch.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
 #pragma once
 
-#define NOMINMAX
+#include "targetver.h"
 
-#include <functional>
+#define NOMINMAX 1
+#define WIN32_LEAN_AND_MEAN 1
+#define WINRT_LEAN_AND_MEAN 1
+
+// Windows Header Files
+#include <windows.h>
+#undef GetCurrentTime
 #include <unknwn.h>
 
+// WinRT Header Files
+#include <winrt/base.h>
+#include <CppWinRTIncludes.h>
 #include <winrt/Microsoft.ReactNative.h>
+
+// C RunTime Header Files
+#include <malloc.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <tchar.h>
+
+// Reference additional headers your project requires here
+#include <functional>
 #include <winrt/Windows.ApplicationModel.Core.h>
 #include <winrt/Windows.Data.Json.h>
 #include <winrt/Windows.Foundation.Collections.h>
