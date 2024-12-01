6cbfc54f1f1e8bfa556251581e44673f4e81f4f7
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2024-12-01 10:30:28 +0100

    bump deps + example move

--- packages/default-storage/example/scripts/android_e2e.sh
+++ packages/default-storage/example/scripts/android_e2e.sh
@@ -3,20 +3,20 @@
 build_apk() {
   echo
   echo "[Android E2E] Building APK"
-  (cd example/android; ./gradlew assembleRelease -PAsyncStorage_useNextStorage=false --max-workers 2)
+  (cd android; ./gradlew assembleRelease -PAsyncStorage_useNextStorage=false --max-workers 2)
 }
 
 bundle_js() {
   extraArgs="$@"
   echo
   echo "[Android E2E] Bundling JS"
-  react-native bundle --entry-file index.ts --platform android --bundle-output example/index.android.jsbundle --dev false $extraArgs
+  react-native bundle --entry-file index.ts --platform android --bundle-output index.android.jsbundle --dev false $extraArgs
 }
 
 
 run_e2e_test() {
   echo "[Android E2E] Running tests"
-  wdio run example/__tests__/android.conf.ts
+  wdio run __tests__/android.conf.ts
 }
 
 case $1 in
